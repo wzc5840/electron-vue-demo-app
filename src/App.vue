f6f41ad2cb9c9293f8bfe1f30f17cf2837005310
Symond <wzc5840@gmail.com>
2024-05-27 16:01:11 +0900

    Init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello Electron with Vue 3 and Vite!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [vue()],
+    base: './', // 确保正确的基础路径
+    publicPath: './',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 8080,
+        open: false,
+    }
+})
